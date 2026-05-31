bd3d7e4507c196282749b6229394ded96952d11b
danylaocruz-ux <danylaocruz@gmail.com>
2026-05-30 22:14:54 -0300

    add backend/app/models/__init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,8 @@
+from .user import User
+from .book import Book
+from .character import Character
+from .audio_segment import AudioSegment
+from .reading_session import ReadingSession
+from .bookmark import Bookmark
+
+__all__ = ["User", "Book", "Character", "AudioSegment", "ReadingSession", "Bookmark"]
